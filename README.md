e538f6a6a91949d223ef5c5f44e8554c66205993
Bengt Brodersen <bengt.brodersen@gmail.com>
2023-08-31 10:36:32 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# chrome-tab-to-the-right-extension
+This extension will add a shortcut to open a new tab to the right side of current active one.
+
+[![Available in the Chrome Web Store](https://storage.googleapis.com/chrome-gcs-uploader.appspot.com/image/WlD8wC6g8khYWPJUsQceQkhXSlv1/tbyBjqi7Zu733AAKA5n4.png "Available in the Chrome Web Store")](https://chrome.google.com/webstore/detail/tab-to-the-right/nibkmpldcbncnpebkehcfibdljlmnafi)
+
+#### Shortcuts
+* New Tab to the Right
+  * **Alt T**
+  * `MacOS` **⌥ T**
+  * ⚠ To overwrite the default 'New Tab' shortcut with this extension by setting the extension shortcut to the default shortcut at chrome://extensions/shortcuts
+
+#### Build
+`zip -r -j tab-to-the-right.zip *`
+
+#### Release
+Upload at https://chrome.google.com/webstore/developer/dashboard
+
---
+++ background.js
@@ -0,0 +1,30 @@
+async function newTab() {
+    const activeTab = await getActiveTab()
+    return chrome.tabs.create({
+        openerTabId: activeTab.id,
+        index: activeTab.index + 1,
+        active: true,
+    })
+}
+
+async function getActiveTab() {
+    return (await chrome.tabs.query({currentWindow: true, active: true}))[0];
+}
+
+// -----------------------------------------------------------------------------
+
+chrome.commands.onCommand.addListener(async (command) => {
+  console.debug(`command: ${command}`)
+  switch (command) {
+    case 'new-tab':
+        return newTab()
+  }
+})
+
+chrome.runtime.onInstalled.addListener((details) => {
+    if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+        chrome.tabs.create({
+            url: "chrome://extensions/shortcuts"
+        })
+    }
+})
